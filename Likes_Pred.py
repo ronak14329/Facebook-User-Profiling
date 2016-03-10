878c31fc188952ec4e8e0d62736ddb3f923b6205
Madhuri Illuri <madhuri.illuri@gmail.com>
2016-03-10 12:53:48 -0800

    Predictions Based on KFold cross validation

--- Likes_Pred.py
+++ Likes_Pred.py
@@ -9,6 +9,10 @@ from sklearn.metrics import accuracy_score
 from sklearn import neighbors
 from sklearn.naive_bayes import GaussianNB
 from sklearn import svm
+
+from sklearn.cross_validation import train_test_split
+from sklearn import cross_validation
+
 #for merging "extracted 'featureset.csv' with 'profile.csv'
 
 #print "90"
@@ -17,6 +21,24 @@ from sklearn import svm
 #b = b.dropna(axis=1)
 #merged = a.merge(b, on='userid')
 #merged.to_csv("outputM.csv", index=False)
+
+
+def k_fold_cross_validation(X, K, randomise = False):
+	"""
+	Generates K (training, validation) pairs from the items in X.
+
+	Each pair is a partition of X, where validation is an iterable
+	of length len(X)/K. So each training iterable is of length (K-1)*len(X)/K.
+
+	If randomise is true, a copy of X is shuffled before partitioning,
+	otherwise its order is preserved in training and validation.
+	"""
+	if randomise: from random import shuffle; X=list(X); shuffle(X)
+	for k in xrange(K):
+		training = [x for i, x in enumerate(X) if i % K != k]
+		validation = [x for i, x in enumerate(X) if i % K == k]
+		yield training, validation
+
 
 
 def get_age_group(age):
@@ -44,6 +66,12 @@ def get_age_label(age):
     else:
         return 1
 
+def padding2(v,t_labels):
+   while(v>0):
+       t_labels.append(2)
+       v=v-1
+   return t_labels    
+   
 def get_images_labels(path,index,training_set):    
      print "getting features and labels..."
      labels = []
@@ -81,6 +109,7 @@ def get_images_labels(path,index,training_set):
                                        
      return features,labels,count 
 
+ 
 def get_testdata(path,index,training_set):
      print "getting test features and labels..."
     
@@ -118,17 +147,55 @@ def get_testdata(path,index,training_set):
                                        
      return features,labels,count
 
-
-
+def padding(v,labels):
+   while(v>0):
+      labels.append(1)
+      v = v-1   
+   return labels
 if __name__ == "__main__":
 	print "enter path to the 'Merge.csv' file, which is uploaded on Github..."
         path = raw_input("enter path to the 'Merge.csv' file, which is uploaded on Github...\n")
         print path
-	traningset = (9501) * 70/100
+    
+        #for doing 70- 30% split....
+        traningset=(9501)*70/100
+        #features,t_fet,labels,t_labels = train_test_split(features, labels, test_size=0.30)      
+        
 	#Predictions for age...
-	features,labels,count = get_images_labels(path,191,traningset) #index 191 denotes "age coulmn" in the outputM.csv data set....and 192 denotes gender column....
+	features,labels,count = get_images_labels(path,191,9501) #index 191 denotes "age coulmn" in the outputM.csv data set....and 192 denotes gender column....
+        
+        features.remove([])      
+        
+        cv = cross_validation.KFold(len(features), n_folds=5, random_state=None)
+        accuracy = 0.0 #intailize accuracy to zero value..
+        for traincv, testcv in cv:
+             print "Doing K fold training.."             
+             training_set = features[traincv[0]:traincv[len(traincv)-1]]
+             labels= labels[traincv[0]:traincv[len(traincv)-1]]
+             x=len(training_set)
+             y=len(labels)
+             dif = x-y
+             labels= padding(dif,labels)      
+             testing_set = features[testcv[0]:testcv[len(testcv)-1]]
+             t_labels = labels[testcv[0]:testcv[len(testcv)-1]]
+             x=len(testing_set)
+             y=len(t_labels)
+             dif = x-y
+             t_labels = padding2(dif,t_labels)      
+             randForest = RandomForestClassifier(n_estimators = 500, n_jobs = 1)
+             randForest.fit(training_set,labels)
+             predFeature = randForest.predict(testing_set)
+	     accuracy += accuracy_score(t_labels,predFeature)
+        accuracy = (accuracy)/5
+	print ("Predicted model Average of 5 fold accuracy for Age :"+str(accuracy)) 
+     
+        #features,t_fet,labels,t_labels = train_test_split(features, labels, test_size=0.30,random_state=42)
+        #x=len(features)
+        #print x
+
 	# outpjutM.csv contains the dataset information which we have extracted from Facebook graph API...
 	#test data ...
+        features,labels,count = get_images_labels(path,191,traningset)
 	t_fet,t_labels,t_count = get_testdata(path,191,traningset)
 
 	#applying random forest classifier.....
@@ -143,8 +210,9 @@ if __name__ == "__main__":
 
 
 	#predictions for gender......
-	features,labels,count = get_images_labels(path,192,traningset) #index 191 denotes "age coulmn" in the outputM.csv data set....and 192 denotes gender column....
-
+               
+	features,labels,count = get_images_labels(path,192,traningset) #index 192 denotes "gender coulmn" in the outputM.csv data set....and 192 denotes gender column....
+        
 	# outpjutM.csv contains the dataset information which we have extracted from Facebook graph API...
 	#test data ... 
 	t_fet,t_labels,t_count = get_testdata(path,192,traningset)
